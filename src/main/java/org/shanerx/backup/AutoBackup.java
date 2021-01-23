d553a67eff593d75a789202f7c8f0a0938616b0c
Sparkling Comet <apps@shanerx.org>
2021-01-23 19:43:56 +0100

    Add LICENSE

--- src/main/java/org/shanerx/backup/AutoBackup.java
+++ src/main/java/org/shanerx/backup/AutoBackup.java
@@ -1,9 +1,24 @@
+/*
+   Copyright 2021 SparklingComet/ShanerX @ www.shanerx.org
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package org.shanerx.backup;
 
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.zeroturnaround.zip.NameMapper;
 import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.File;
